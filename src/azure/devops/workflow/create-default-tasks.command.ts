68459ca2ec1e01b22979d6fa3f3d041823d6aa0a
tacosontitan <contact@tacosontitan.com>
2025-04-17 21:27:34 -0500

    Integrate with devops to create tasks

--- src/azure/devops/workflow/create-default-tasks.command.ts
+++ src/azure/devops/workflow/create-default-tasks.command.ts
@@ -1,9 +1,13 @@
 import * as vscode from 'vscode';
+import * as devops from "azure-devops-node-api";
 
 import { PreDefinedTask } from './pre-defined-task';
 import { WorkItemType } from '../work-item-type';
 import { DevOpsCommand } from '../devops-command';
 import { IAssistant } from '../../../assistant';
+import { WebApi } from 'azure-devops-node-api/WebApi';
+import { JsonPatchDocument, Operation } from 'azure-devops-node-api/interfaces/common/VSSInterfaces';
+import { WorkItemTrackingApi } from 'azure-devops-node-api/WorkItemTrackingApi';
 
 /**
  * Represents a {@link DevOpsCommand} that creates pre-defined tasks representing the typical workflow of a work item in Azure DevOps.
@@ -56,21 +60,75 @@ export class CreateDefaultTasksCommand
 		const personalAccessToken = this.getPersonalAccessToken();
 		if (!personalAccessToken) {
 			vscode.window.showErrorMessage('This command requires a personal access token (PAT) to be configured.');
-			return Promise.resolve();
+			return;
 		}
+
+		const organizationUri = this.getOrganizationUri();
+		if (!organizationUri) {
+			vscode.window.showErrorMessage('This command requires an Azure DevOps organization to be configured.');
+			return;
+		}
+
+		const projectName = this.getProjectName();
+		if (!projectName) {
+			vscode.window.showErrorMessage("Azure DevOps project is not configured. Commands that require it will not work.");
+			return;
+		}
+
+		const authenticationHandler = devops.getPersonalAccessTokenHandler(personalAccessToken);
+		const connection = new WebApi(organizationUri, authenticationHandler);
+		const workItemTrackingClient: WorkItemTrackingApi = await connection.getWorkItemTrackingApi();
 
 		const workItemNumberResponse = await vscode.window.showInputBox({ prompt: 'Enter the work item number for which you want to create default tasks.' });
 		const workItemNumber = parseInt(workItemNumberResponse ?? '-1');
 		if (isNaN(workItemNumber) || workItemNumber <= 0) {
 			vscode.window.showErrorMessage('Work item number is required to create default tasks.');
-			return Promise.resolve();
+			return;
 		}
 
-		vscode.window.showInformationMessage(`Creating default tasks for work item # ${workItemNumber}...`);
-		vscode.window.showWarningMessage('This feature is not yet implemented. Default tasks will not be created.');
-		vscode.window.showInformationMessage('Your configured PAT is: ' + personalAccessToken);
+		vscode.window.showInformationMessage(`Creating default tasks for work item #${workItemNumber} in project '${projectName}'...`);
 		for (const task of CreateDefaultTasksCommand.defaultTasks) {
-			assistant.writeLine(`Creating task '${task.name}' with '${task.remainingWork}' hours of remaining work.`);
+			const patchDocument: JsonPatchDocument = [
+				{
+					op: Operation.Add,
+					path: '/fields/System.Title',
+					value: task.name,
+				},
+				{
+					op: Operation.Add,
+					path: '/fields/Microsoft.VSTS.Scheduling.RemainingWork',
+					value: task.remainingWork,
+				},
+				{
+					op: Operation.Add,
+					path: '/fields/System.Description',
+					value: task.description,
+				},
+				{
+					op: Operation.Add,
+					path: '/relations/-',
+					value: {
+						rel: 'System.LinkTypes.Hierarchy-Reverse',
+						url: `${organizationUri}/_apis/wit/workItems/${workItemNumber}`,
+						attributes: {
+							comment: 'Task created by Hazel\'s Toolbox',
+						},
+					},
+				},
+			];
+
+			try {
+				const createdTask = await workItemTrackingClient.createWorkItem(
+					[],
+					patchDocument,
+					projectName,
+					'Task'
+				);
+				assistant.writeLine(`Created task '${task.name}' with ID ${createdTask.id} under work item #${workItemNumber}.`);
+			} catch (error) {
+				const errorMessage = (error as Error).message;
+				vscode.window.showErrorMessage(`Failed to create task '${task.name}': ${errorMessage}`);
+			}
 		}
 
 		return Promise.resolve();
