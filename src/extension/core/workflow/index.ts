3873b9a16433d353d23577bafb1a4a3c998e9fa9
tacosontitan <contact@tacosontitan.com>
2025-07-09 18:01:29 -0500

    Remove over complication of transactions

--- src/extension/core/workflow/index.ts
+++ src/extension/core/workflow/index.ts
@@ -1,7 +1,4 @@
-export * from "./transaction";
-export * from "./transactional-service";
 export * from "./work-item";
 export * from "./work-item-state";
 export * from "./work-item-type";
 export * from "./work-item.service";
-
