3d2520bca035b6def988b27b8ccfd6cd6a431791
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2026-03-04 21:08:46 -0600

    Remove test comment

--- src/extension/core/command-registry.ts
+++ src/extension/core/command-registry.ts
@@ -110,7 +110,6 @@ export class CommandRegistry {
 		configurationProvider: IConfigurationProvider,
 		workItemService: IWorkItemService
 	) {
-		// test
 		const commands = [
 			new SetTaskStateToNewCommand(secretProvider, configurationProvider, tasksTreeProvider, workItemService),
 			new SetTaskStateToActiveCommand(secretProvider, configurationProvider, tasksTreeProvider, workItemService),
